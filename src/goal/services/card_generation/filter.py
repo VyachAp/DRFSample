eac49523311199a406db7203a2704fb73d370d4c
VyachAp <43495346+VyachAp@users.noreply.github.com>
2023-04-06 10:11:50 +0200

    Translate comments

--- src/goal/services/card_generation/filter.py
+++ src/goal/services/card_generation/filter.py
@@ -1,14 +1,3 @@
-"""Фильтрация списка сотрудников для включении в годовое целеполагание.
-
-В соответствии со схемой[1] необходимо решить, будет ли сотрудник
-включён в список сотрудников для годового целеполагания. Схема
-представлена различными ветвями решений, на основании которых
-должно быть принято решение о включении или исключении сотрудника из
-участия в годовом целеполагании.
-
-[1] https://wiki.x5.ru/pages/viewpage.action?pageId=270917231
-"""
-
 import abc
 from datetime import date, datetime
 from functools import lru_cache
@@ -21,24 +10,14 @@ from .consts import OrganizationMethod
 
 class FilterEmployee:
     """
-    Сервис фильтрации сотрудника для определения участия в целеполагании в конкретном периоде.
-
-    Определение участия сотрудника должно происходить автоматически
-    на основании схема определения участника целеполагания и цепочки
-    фильтров, которые принимают решение о включении сотрудника.
-
-    Эволюционно пришли к тому, что фильтрация необходима и возможна только на финальном этапе.
-    Условия, касающиеся бонусов проверены ранее в src.goal.services.card_generation.bonus_condition.
-    Осталось проверить соответствие на должность и дату приема
+    We need to check out if employee is suitable for card generation with hise historical records slice
     """
 
     def __init__(self, dates: Dict, employee_records: List[Dict], period):
         """
-        dates - Это даты, пришедшие к нам после применения к записям сотрудника дат действия бонусов
-                {"start": Дата начала, "end": Дата окончания}
+        dates - {"start": Date start, "end": Date end}
 
-        employee_records - Это срез исторических записей по пользователю, которые задействованы сейчас
-
+        employee_records - List of employee historical records
         """
         self.dates = dates
         self.employee_records = employee_records
@@ -61,13 +40,6 @@ class FilterEmployee:
 
 
 class SuitableFilter(abc.ABC):
-    """
-    Базовый класс для ветвей фильтрации сотрудника.
-
-    Каждый класс фильтра должен принять решение для своей области
-    принятия решений: включать сотрудника или нет. Класс принимает
-    решение на основании информации о самом сотруднике.
-    """
 
     def __init__(self, employee: List[Dict], period: Period, dates: Dict):
         self.employee = employee
@@ -130,8 +102,7 @@ class PositionStatusFilter(SuitableFilter):
         return self._hired_at()
 
     def _hired_at(self) -> bool:
-        # Если был принят на работу не позднее даты окончания генерации карт
-        # Для определенности берем [0], но в каждой записи из employee есть hire_dt
+        # All hire_dt in self.employee are the same, so take the first one as definition mark
         return (
             date.fromisoformat(self.employee[0]["hire_dt"])
             <= self.period.cards_generation_end_date
